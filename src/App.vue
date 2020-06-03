c83752693a26d9864b19d61ad0144ce6959b2b72
Rheidner <rheidner.achiles@gmail.com>
2020-06-03 15:24:44 -0300

    init: adiciona estilos base e imagem base para o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <nav id="nav">
+        <ul>
+          <li class="nav-item">
+            <router-link to="/">
+              <img class="logo" src="./assets/build-a-bot-logo.png">
+              Build a Bot
+            </router-link>|
+          </li>
+          <li class="nav-item">
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
+main {
+  margin: 0 auto;
   padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
