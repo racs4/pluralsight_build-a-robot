c94c4e047949c00d2a3749c3fb2b8178ac24ea65
Rheidner <rheidner.achiles@gmail.com>
2020-06-08 16:03:07 -0300

    feat: adiciona pagina para visualizacao de todas as partes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import VueRouter from 'vue-router';
 import HomePage from '@/home/HomePage.vue';
 import RobotBuilder from '@/build/RobotBuilder.vue';
 import PartInfo from '@/parts/PartInfo.vue';
+import BrowseParts from '@/parts/BrowseParts.vue';
+import RobotArms from '@/parts/RobotArms.vue';
+import RobotHeads from '@/parts/RobotHeads.vue';
+import RobotTorsos from '@/parts/RobotTorsos.vue';
+import RobotBases from '@/parts/RobotBases.vue';
 
 Vue.use(VueRouter);
 
@@ -24,6 +29,33 @@ const routes = [
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'amrs',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   },
   {
     path: '/parts/:partType/:id',
