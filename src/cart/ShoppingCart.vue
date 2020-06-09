9f1374d7c358de08c490c2cd288971555b756351
Rheidner <rheidner.achiles@gmail.com>
2020-06-09 15:55:33 -0300

    feat: adiciona filtro para valor monetario

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -18,7 +18,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost}}
+            {{robot.cost | currency('$')}}
           </td>
         </tr>
       </tbody>
@@ -41,7 +41,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost}}
+            {{robot.cost | currency('$')}}
           </td>
         </tr>
       </tbody>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import pinDirective from '@/shared/pin-directive';
+import currencyFilter from '@/shared/currency-filter';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
 Vue.directive('pin', pinDirective);
+Vue.filter('currency', currencyFilter);
 
 new Vue({
   router,
