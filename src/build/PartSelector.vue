ceef3ca79340e9a6cf9ad03d09b99d0b33947c0f
Rheidner <rheidner.achiles@gmail.com>
2020-06-09 15:46:06 -0300

    feat: adiciona diretiva para cuidar do simbolo 'sale'

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,7 +3,8 @@
     <img @click="viewPartDescription" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{bottom: '10px', right: '5px'}"
+      class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -83,9 +84,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-param-reassign */
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+
+  element.style.position = 'absolute';
+}
